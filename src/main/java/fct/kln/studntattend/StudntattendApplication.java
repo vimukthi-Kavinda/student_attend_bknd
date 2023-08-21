f1fba25658443066404cf90c0fcc8a3809637a13
Vimukthi_k <vimukthi2017kavinda@gmail.com>
2023-08-22 02:45:15 +0530

    init commit

---
+++ src/main/java/fct/kln/studntattend/StudntattendApplication.java
@@ -0,0 +1,13 @@
+package fct.kln.studntattend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudntattendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudntattendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fct/kln/studntattend/StudntattendApplicationTests.java
@@ -0,0 +1,13 @@
+package fct.kln.studntattend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudntattendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
